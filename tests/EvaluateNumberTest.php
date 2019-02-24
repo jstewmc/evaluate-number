f7fe8b01f7180bc7935321baf1554fc0937c340c
Jack Clayton <clayjs0@gmail.com>
2019-02-24 11:29:54 -0700

    Fix failing tests

--- tests/EvaluateNumberTest.php
+++ tests/EvaluateNumberTest.php
@@ -17,149 +17,149 @@ use Jstewmc\TestCase\TestCase;
 class EvaluateNumberTest extends TestCase
 {
 	/* !bool */
-	
+
 	/**
 	 * invoke() should return integer if value is a bool
 	 */
 	public function testInvokeReturnsIntegerIfValueIsBool()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())(true));
-		
+
 		return;
 	}
-	
-	
+
+
 	/* !float */
-	
+
 	/**
 	 * invoke() should return float if value is a float
 	 */
 	public function testInvokeReturnsFloatIfValueIsFloat()
 	{
 		$this->assertEquals(1.1, (new EvaluateNumber())(1.1));
-		
+
 		return;
 	}
-	
-	
+
+
 	/* !int */
-	 
+
 	/**
 	 * invoke() should return integer if value is an integer
 	 */
 	public function testInvokeReturnsIntegerIfValueIsInteger()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())(1));
-		
+
 		return;
 	}
-	
-	
+
+
 	/* !string */
-	 
+
 	/**
 	 * invoke() should return float if value is a string float
 	 */
 	public function testInvokeReturnsFloatIfValueIsStringFloat()
 	{
 		$this->assertEquals(1.0, (new EvaluateNumber())('1.0'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return integer if value is a string integer
 	 */
 	public function testInvokeReturnsIntegerIfValueIsStringInteger()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())('1'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return float if value is a fraction
 	 */
 	public function testInvokeReturnsFloatIfValueIsFraction()
 	{
 		$this->assertEquals(0.5, (new EvaluateNumber())('1/2'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return float if value is a mixed number
 	 */
 	public function testInvokeReturnsFloatIfValueIsMixedNumber()
 	{
 		$this->assertEquals(1.5, (new EvaluateNumber())('1 1/2'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return integer if value is a comma-separated integer
 	 */
 	public function testInvokeReturnsIntegerIfValueIsCommaSeparatedInteger()
 	{
 		$this->assertEquals(1000, (new EvaluateNumber())('1,000'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return an integer if value is a comma-separated float
 	 */
 	public function testInvokeReturnsFloatIfValueIsCommaSeparatedFloat()
 	{
 		$this->assertEquals(1000.5, (new EvaluateNumber())('1,000.5'));
-		
+
 		return;
 	}
-	
-	/** 
+
+	/**
 	 * invoke() should return int if value has suffix
 	 */
 	public function testInvokeReturnsIntIfValueHasSuffix()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())('1st'));
-		
+
 		return;
 	}
-	
-	/** 
+
+	/**
 	 * invoke() should return int if value is a cardinal string (e.g., "one")
 	 */
 	public function testInvokeReturnsIntIfValueIsCardinalString()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())('one'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return int if value is an ordinal string (e.g., "first")
 	 */
 	public function testInvokeReturnsIntIfValueIsAnOrdinalString()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())('first'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return int if value is an short integer name
 	 */
 	public function testInvokeReturnsIntIfValueIsShortName()
 	{
 		$this->assertEquals(
-			111, 
+			111,
 			(new EvaluateNumber())('one hundred and eleven')
 		);
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return int if value is a medium integer name
 	 */
@@ -169,12 +169,12 @@ class EvaluateNumberTest extends TestCase
 			111111,
 			(new EvaluateNumber())(
 				'one hundred eleven thousand, one hundred and eleven'
-			) 
+			)
 		);
-		
-		return; 
+
+		return;
 	}
-	
+
 	/**
 	 * invoke() should return int if value is a long-length integer name
 	 */
@@ -186,10 +186,10 @@ class EvaluateNumberTest extends TestCase
 	 			'one million one hundred eleven thousand one hundred and eleven'
 	 		)
 	 	);
-	 	
+
 	 	return;
 	}
-	
+
 	/**
 	 * invoke() should return float if value is integer percent
 	 *
@@ -198,63 +198,63 @@ class EvaluateNumberTest extends TestCase
 	public function testInvokeReturnsFloatIfValueIsIntegerPercent()
 	{
 		$this->assertEquals(0.01, (new EvaluateNumber())('1%'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return float if value is float percent
 	 */
 	public function testInvokeReturnsFloatIfValueIsFloatPercent()
 	{
 		$this->assertEquals(0.015, (new EvaluateNumber())('1.5%'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return int if value is integer dollars
 	 */
 	public function testInvokeReturnsIntIfValueIsIntegerDollars()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())('$1'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return float if value is float dollars
 	 */
 	public function testInvokeReturnsFloatIfValueIsFloatDollars()
 	{
 		$this->assertEquals(1000.5, (new EvaluateNumber())('$1,000.50'));
-		
+
 		return;
 	}
-	
+
 	/**
 	 * invoke() should return zero if value is non-numeric string
 	 */
 	public function testInvokeReturnsZeroIfValueIsNonNumericString()
 	{
 		$this->assertEquals(0, (new EvaluateNumber())('foo'));
-		
+
 		return;
 	}
-	
-	
+
+
 	/* !array */
-	
+
 	/**
 	 * invoke() should return zero if value is an empty array
 	 */
 	public function testInvokeReturnsZeroIfValueIsEmptyArray()
 	{
 		$this->assertEquals(0, (new EvaluateNumber())([]));
-		
+
 		return;
 	}
-	
+
 	/**
 	* invoke() should return one if value is a non-empty array
 	*/
@@ -264,17 +264,17 @@ class EvaluateNumberTest extends TestCase
 
 		return;
 	}
-	
-	
+
+
 	/* !object */
-	
+
 	/**
 	 * invoke() should return one if value is an object
 	 */
 	public function testInvokeReturnsOneIfValueIsObject()
 	{
 		$this->assertEquals(1, (new EvaluateNumber())(new \StdClass()));
-		
+
 		return;
 	}
 }
